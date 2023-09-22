04e0a1fdcbf70333f5e97325dadda025280d51a6
juanm <juanm@Carlos>
2023-09-22 16:29:37 -0500

    First commit

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ *
+* ITESS-TICS
+ * Taller de investigacion
+ *Intento de Curso de certificacion java 0Z1-808
+ *By FMM
+ * 22/Septiembre/2023
+ */
+package Main;
+
+/**
+ *
+ * @author juanm
+ */
+public class Main {
+    public static void main(String ...arg){
+        
+    }
+}
